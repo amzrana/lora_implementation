4fa1cad0f08286945905811f4692684f74a4a3f7
Aman Rana <amanrana20@icloud.com>
2023-07-30 01:15:29 +0200

    Initial code for LoRA and DyLoRA

---
+++ lora.py
@@ -0,0 +1,66 @@
+import torch
+import torch.nn as nn
+
+class LoRALayer(object):
+    def __init__(self, lora_rank: int, lora_alpha: int):
+        self.lora_rank = lora_rank
+        self.lora_alpha = lora_alpha
+
+
+class LoraModule(nn.Module):
+    def __init__(
+        self,
+        input_dim: int,
+        output_dim: int,
+        lora_rank: int,
+        alpha: int,
+    ):
+        super().__init__()
+        self._lora_rank = lora_rank
+        self._alpha = alpha
+
+        self.A = nn.Linear(input_dim, lora_rank)
+        self.B = nn.Linear(lora_rank, output_dim)
+
+    @property
+    def lora_rank(self):
+        return self._lora_rank
+
+    @property
+    def alpha(self):
+        return self._alpha
+
+    def foward(self, x: torch.Tensor) -> torch.Tensor:
+        return self._alpha * (
+            self.B.transpose(1, 0) @ self.A.transpose(1, 0) @ x
+        )
+
+
+
+class DynamicLoraModule(nn.Module):
+    def __init__(
+        self,
+        input_dim: int,
+        output_dim: int,
+        lora_maximum_rank: int,
+        alpha: int,
+    ):
+        super().__init__()
+        
+        self._lora_maximum_rank = lora_maximum_rank
+        self._alpha = alpha
+
+        self.A = nn.Linear(input_dim, lora_maximum_rank)
+        self.B = nn.Linear(lora_maximum_rank, output_dim)
+
+        # Setting default current rank
+        self._current_rank = 0
+    
+    def get_current_rank(self):
+        return self._current_rank
+    
+    def set_current_rank(self, n: int):
+        self._current_rank = n
+
+    def forward(self):
+        pass
